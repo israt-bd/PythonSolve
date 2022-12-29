51b5f9f67204ca6210e8281dbe24bdb966461ac8
israt-bd <israt15-10474@diu.edu.bd>
2022-12-30 04:27:37 +0600

    upload1

---
+++ 17th.py
@@ -0,0 +1,12 @@
+y = input()
+x = int(y)
+
+if x<=100:
+        print("Yes in 100")
+elif x<= 1000:
+        print("Yes in 1000")
+elif x<= 2000:
+        print("Yes in 2000")
+
+else :
+        print("No")
