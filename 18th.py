0f0f20afeec222651220c8b79c35fbb47a6edc9b
israt-bd <israt15-10474@diu.edu.bd>
2022-12-30 04:47:00 +0600

    upload1

---
+++ 18th.py
@@ -0,0 +1,20 @@
+import math
+a = input()
+b = input()
+c = input()
+
+p = int(a)
+q = int(b)
+r = int(c)
+
+
+z = (p+q+r)
+
+if p == q and q==r :
+    o =(p+q+r)
+    l = int(o*3)
+    print(l)
+else :
+    print(z)
+
+
